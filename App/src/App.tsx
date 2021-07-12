a39415552ae9ad214fba2cf40f74327a2121c6dc
Audrey Kadjar <audreykadjar@gmail.com>
2021-07-12 09:22:17 +0200

    set up redux

--- App/src/App.tsx
+++ App/src/App.tsx
@@ -1,50 +1,60 @@
 import React, {useState} from 'react';
-import { SafeAreaView, Button } from 'react-native';
+import {SafeAreaView, Button} from 'react-native';
 import Auth0 from 'react-native-auth0';
-import {Login} from './components/Auth/Login'
+import {Login} from './components/Auth/Login';
+import {connect} from 'react-redux';
+import {StateModel} from './reducers';
 
+const credentials = require('./auth0-configuration.ts').default;
+const auth0 = new Auth0(credentials);
 
- const credentials = require('./auth0-configuration.ts').default;
- const auth0 = new Auth0(credentials);
+const mapStateToProps = (state: StateModel) => ({
+  state
+});
 
-
-
+const mapDispatchToProps = (dispatch:any) => ({
+  dispatch
+});
 
 const App = () => {
-
   const [accessToken, setAccessToken] = useState<string | null>(null);
-  const [userLoggedOut, setUserLoggedOut] = useState(false)
+  const [userLoggedOut, setUserLoggedOut] = useState(false);
 
-   const logout = () => {
-  auth0.webAuth
+  const logout = () => {
+    auth0.webAuth
       .clearSession({federated: false})
       .then(success => {
-          setAccessToken(null);
-          setUserLoggedOut(true)
+        setAccessToken(null);
+        setUserLoggedOut(true);
       })
       .catch(error => {
-          console.log('Log out cancelled');
+        console.log('Log out cancelled');
       });
+  };
+
+  const changeAccessToken = (token: string | null) => {
+    setAccessToken(token);
+  };
+
+  return (
+      <SafeAreaView style={{flex: 1}}>
+        {accessToken && (
+          <Button
+            onPress={logout}
+            title="LOG OUT"
+            color="#841584"
+            accessibilityLabel="logout"
+          />
+        )}
+
+        {!accessToken && (
+          <Login
+            changeAccessToken={changeAccessToken}
+            userLoggedOut={userLoggedOut}
+          />
+        )}
+      </SafeAreaView>
+  );
 };
 
-const changeAccessToken = (token: string | null) => {
-  setAccessToken(token)
-}
-
-   return (
-     <SafeAreaView style={{ flex: 1}}>
-
-       {accessToken && (
-          <Button onPress={logout} title="LOG OUT" color="#841584" accessibilityLabel="logout" />
-       )}
-
-
-       {!accessToken && (
-         <Login changeAccessToken={changeAccessToken} userLoggedOut={userLoggedOut} />
-       )}
-       
-     </SafeAreaView>
-   );
- };
-
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- index.js
+++ index.js
@@ -1,11 +1,19 @@
-/**
- * @format
- */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App/src/App';
 import {name as appName} from './App.json';
+import {connect} from 'react-redux';
+import {Provider} from 'react-redux';
+import {store} from './App/src/store';
 
 
+AppComponent = connect()(App)
 
-AppRegistry.registerComponent(appName, () => App);
+const AiryMobileApp = () => (
+  <Provider store={store}>
+    <AppComponent />
+  </Provider>
+)
+
+
+AppRegistry.registerComponent(appName, () => AiryMobileApp);
