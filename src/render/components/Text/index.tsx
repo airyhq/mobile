8f82caee87544807d43233592887475e150609c9
Audrey Kadjar <audreykadjar@gmail.com>
2021-08-18 18:19:16 +0200

    added components for providers

--- src/render/components/Text/index.tsx
+++ src/render/components/Text/index.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import Linkify from 'linkifyjs/react';
-import styles from './index.module.scss';
+import {StyleSheet} from 'react-native';
+
+//import Linkify from 'linkifyjs/react';
 
 type TextRenderProps = {
   text: string;
   fromContact?: boolean;
 };
+
+//https://www.npmjs.com/package/react-native-hyperlink
 
 export const Text = ({text, fromContact}: TextRenderProps) => (
   <Linkify
@@ -18,3 +21,27 @@ export const Text = ({text, fromContact}: TextRenderProps) => (
     {text}
   </Linkify>
 );
+
+
+const styles = StyleSheet.create({
+  contactContent: {
+    width: width,
+    height: height,
+    backgroundColor: 'white',
+  },
+  memberContent: {
+    flex: 0.5,
+    backgroundColor: 'blue'
+  },
+  messageLink: {
+  backgroundColor: 'blue'
+},
+contactContent: {
+  background: var(--color-background-blue);
+  color: var(--color-text-contrast);
+}, 
+memberContent: {
+  background: var(--color-airy-blue);
+  color: white;
+}
+});
