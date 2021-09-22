2361c85bcf7124dff4511adf1c73b8e7c6081ff8
Audrey Kadjar <audreykadjar@gmail.com>
2021-09-22 18:11:44 +0200

    fixing text encoding bug

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
 
+// /!/ removing this import causes errors
+import * as encoding from 'text-encoding';
+
 AppRegistry.registerComponent(appName, () => App);
