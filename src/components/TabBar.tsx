728d25e50e956e2ffcf2e5da016c91fad03c4b97
thorstenairy <stayprod.io@gmail.com>
2021-09-27 15:58:21 +0200

    first UI changes

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {ConversationList} from '../views/inbox/ConversationList';
 import SettingsView from '../views/settings';
 import InboxIcon from '../assets/images/icons/bubble_icon.svg';
 import SettingsIcon from '../assets/images/icons/settings_icon.svg';
+import FilterIcon from '../assets/images/icons/filterIcon.svg';
 import {createStackNavigator} from '@react-navigation/stack';
 import MessageList from '../views/inbox/MessageList';
 import {colorAiryBlue, colorTextGray} from '../assets/colors';
 import {Avatar} from './Avatar';
 import {CurrentState} from './CurrentState';
-import {Dimensions, View} from 'react-native';
+import {Animated, Dimensions, View, Text} from 'react-native';
 import IconChannel from './IconChannel';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {TouchableOpacity} from 'react-native-gesture-handler';
 
 export const TabBar = () => {
   const Tab = createBottomTabNavigator();
@@ -59,12 +61,96 @@ export const TabBar = () => {
     );
   };
 
+  const [filterOpen, setFilterOpen] = useState(false);
+  const [headerTitle, setHeaderTitle] = useState('');
+  const [largeTitle, setLargeTitle] = useState(false);
+  const defaultHeaderHeight = 91;
+  const expandedHeaderHeight = 350;
+  const expandAnimation = useRef(
+    new Animated.Value(expandedHeaderHeight),
+  ).current;
+
+  const onCollapse = () => {
+    Animated.timing(expandAnimation, {
+      toValue: expandedHeaderHeight,
+      duration: 400,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const onExpand = () => {
+    Animated.timing(expandAnimation, {
+      toValue: defaultHeaderHeight,
+      duration: 400,
+      useNativeDriver: false,
+    }).start();
+  };
+
+  const toggleFiltering = () => {
+    filterOpen ? onExpand() : onCollapse();
+    !filterOpen ? setHeaderTitle('Filter') : setHeaderTitle('');
+    setFilterOpen(!filterOpen);
+  };
+
+  const applyFilters = () => {
+    setFilterOpen(!filterOpen);
+    console.log('FILTERS APPLIED');
+  };
+
+  const CollapsedFilterView = () => {
+    return (
+      <TouchableOpacity onPress={toggleFiltering}>
+        <FilterIcon height={32} width={32} fill={colorAiryBlue} />
+      </TouchableOpacity>
+    );
+  };
+
+  const ExpandedFilterView = () => {
+    return (
+      <TouchableOpacity
+        onPress={toggleFiltering}
+        style={{
+          position: 'absolute',
+          right: 0,
+          bottom: 10,
+          height: 30,
+          width: 80,
+          alignItems: 'center',
+          justifyContent: 'center',
+          backgroundColor: colorAiryBlue,
+          borderRadius: 8,
+        }}>
+        <Text style={{color: 'white', fontFamily: 'Lato'}}>Apply</Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <Stack.Navigator>
       <Stack.Screen
         name="Inbox"
         component={TabBarScreens}
-        options={{headerShown: false}}
+        options={{
+          headerShown: true,
+          headerTitle: headerTitle,
+          headerTitleStyle: {
+            fontSize: 28,
+            position: 'absolute',
+            left: 0,
+            top: -150,
+            fontFamily: 'Lato',
+          },
+          headerTitleAlign: 'left',
+          headerStyle: {height: expandAnimation, backgroundColor: 'pink'},
+          headerRightContainerStyle: {marginRight: 8},
+          headerRight: () => {
+            return filterOpen ? (
+              <ExpandedFilterView />
+            ) : (
+              <CollapsedFilterView />
+            );
+          },
+        }}
       />
       <Stack.Screen
         name="MessageList"
