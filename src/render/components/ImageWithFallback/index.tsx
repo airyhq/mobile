f10a19e8aa5e7a911fcf25b060c3ea9394c7a127
Audrey Kadjar <audreykadjar@gmail.com>
2021-10-14 17:16:33 +0200

    fixed any on image component

--- src/render/components/ImageWithFallback/index.tsx
+++ src/render/components/ImageWithFallback/index.tsx
@@ -1,11 +1,11 @@
 import React, {useEffect, useState} from 'react';
-import {Image} from 'react-native';
+import {Image, StyleProp, ImageStyle} from 'react-native';
 import {SvgUri} from 'react-native-svg';
 
 type ImageRenderProps = {
   src: string;
   alt?: string;
-  imageStyle?: any;
+  imageStyle?: StyleProp<ImageStyle>;
 };
 
 /**
