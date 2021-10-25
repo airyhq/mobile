b965aaf3db4b66d7a46880b5d5c098d908628736
Aitor Algorta <aitor@airy.co>
2021-10-25 17:34:49 +0200

    fix lint

--- src/views/inbox/ConversationList/index.tsx
+++ src/views/inbox/ConversationList/index.tsx
@@ -80,6 +80,7 @@ export const ConversationList = (props: ConversationListProps) => {
           console.log('error');
         });
     }, 1000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
