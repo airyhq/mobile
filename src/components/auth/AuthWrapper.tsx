c139ca831d2bddd67d9c9cac71adbbb90c3b134f
Audrey Kadjar <audreykadjar@gmail.com>
2021-11-01 12:21:09 +0100

    fixed warning

--- src/components/auth/AuthWrapper.tsx
+++ src/components/auth/AuthWrapper.tsx
@@ -55,20 +55,23 @@ export const AuthWrapper = ({children}) => {
         logout();
       });
 
-      api.getConfig().then(({userProfile}) => {
-        const nextUser = {
-          token,
-          host,
-          name: userProfile.name,
-          avatarUrl: userProfile.avatarUrl,
-        };
-        const realm = RealmDB.getInstance();
-        realm.write(() => {
-          realm.create('UserInfo', nextUser, 'modified');
-        });
-        setUser(nextUser);
-        setIsAuthenticated(true);
-      });
+      api
+        .getConfig()
+        .then(({userProfile}) => {
+          const nextUser = {
+            token,
+            host,
+            name: userProfile.name,
+            avatarUrl: userProfile.avatarUrl,
+          };
+          const realm = RealmDB.getInstance();
+          realm.write(() => {
+            realm.create('UserInfo', nextUser, 'modified');
+          });
+          setUser(nextUser);
+          setIsAuthenticated(true);
+        })
+        .catch(error => error);
     },
     [user, logout],
   );
