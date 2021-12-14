1802500ecbe66ab65b12d0bc73d7abd29093a7bf
Aitor Algorta <aitor@airy.co>
2021-12-14 17:03:31 +0100

    wip

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import OneSignal from 'react-native-onesignal';
 
 // /!/ removing this import causes errors
 import * as encoding from 'text-encoding';
+
+//OneSignal Init Code
+OneSignal.setLogLevel(6, 0);
+OneSignal.setAppId("d3d8d445-1cc5-4c20-8d1d-ea494369b2c9");
+//END OneSignal Init Code
+
+//Prompt for push on iOS
+OneSignal.promptForPushNotificationsWithUserResponse(response => {
+  console.log("Prompt response:", response);
+});
+
+//Method for handling notifications received while app in foreground
+OneSignal.setNotificationWillShowInForegroundHandler(notificationReceivedEvent => {
+  console.log("OneSignal: notification will show in foreground:", notificationReceivedEvent);
+  let notification = notificationReceivedEvent.getNotification();
+  console.log("notification: ", notification);
+  const data = notification.additionalData
+  console.log("additionalData: ", data);
+  // Complete with null means don't show a notification.
+  notificationReceivedEvent.complete(notification);
+});
+
+//Method for handling notifications opened
+OneSignal.setNotificationOpenedHandler(notification => {
+  console.log("OneSignal: notification opened:", notification);
+});
 
 AppRegistry.registerComponent(appName, () => App);
