f8f3eb8caf7c916bdfad14a3fa7a514941ba7c91
Thorsten <thorsten@airy.co>
2022-01-07 15:18:00 +0100

    fixed sdk and gradle

--- android/app/src/main/java/com/mobile/MainApplication.java
+++ android/app/src/main/java/com/mobile/MainApplication.java
@@ -11,6 +11,7 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import com.mkuczera.RNReactNativeHapticFeedbackPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           @SuppressWarnings("UnnecessaryLocalVariable")
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
+            packages.add(new RNReactNativeHapticFeedbackPackage());
           // packages.add(new MyReactNativePackage());
           return packages;
         }
